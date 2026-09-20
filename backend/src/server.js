b15242c46bda1b66e569862a90f3b8569760378f
Olasunkanmi Oguntoyinbo <olasunkanmioguntoyinbo@Olasunkanmis-MacBook-Pro.local>
2026-09-20 23:08:42 +0100

    Enabled CORS for client origins

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,14 +1,23 @@
 const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const connectToDB = require("./config/dbConfig");
+
 const app = express();
 
 // Using dotenv to load environment variables from a .env file into process.env
-const dotenv = require("dotenv");
 dotenv.config();
 
 // Importing all required modules and dependencies
-const connectToDB = require("./config/dbConfig");
-
 connectToDB();
+
+// Enabling CORS for all incoming client origins
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  }),
+);
 
 // Setting up middleware to parse JSON request bodies
 app.use(express.json());
