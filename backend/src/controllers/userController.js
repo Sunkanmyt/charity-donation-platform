5d9e60e9786bbdf1fc51b132ad20ee6c9ee2f5a5
Freedom Ehika <freedomehika1@gmail.com>
2026-09-22 18:23:24 +0100

    restructure user controller to suit sukanmi

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -1,19 +1,6 @@
 const User = require("../models/User");
 const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
-
-const generateToken = (user) => {
-  return jwt.sign(
-    {
-      id: user._id,
-      role: user.role,
-    },
-    process.env.JWT_SECRET,
-    {
-      expiresIn: "7d",
-    },
-  );
-};
+const generateToken = require("../utils/generateToken");
 
 const registerUser = async (req, res) => {
   try {
